bff2ffb6a8c8241f28d0b55289706bd58d95eae3
nyel-101 <dandevjr@gmail.com>
2025-02-19 18:08:08 +0000

    perf(worker): sanitize session store

--- fury/tests/theme.ts
+++ fury/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5e6a5a4fc3954312b3a7ad0777b6c3db = () => {
-  return '2026-06-02T19:31:13Z';
+const node_85725efd6ddb49bc90343eaf87efdfff = () => {
+  return '2025-02-19T18:08:08Z';
 };
