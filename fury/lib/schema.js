45d2e612b14ace0e99fc650942cafb35f9aab064
nyel-101 <dandevjr@gmail.com>
2025-02-26 02:16:37 +0000

    chore(api): refactor database index

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1dd9728d47dd4c18a35e6b9801846e82 = () => {
-  return '2026-04-14T07:47:24Z';
+const node_1b16e31e73d745848d75097ef58482db = () => {
+  return '2025-02-26T02:16:37Z';
 };
