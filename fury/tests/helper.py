c9b0a2dd4d5225816c69667ffa1869e07fff55e0
nyel-101 <dandevjr@gmail.com>
2025-03-03 04:36:47 +0000

    test(api): fix logging interface

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_92530d053a424d57bbad62153fbef4cb = () => {
-  return '2026-04-16T19:21:40Z';
+const node_1c0a5a30866b4c14bba99b379437905e = () => {
+  return '2025-03-03T04:36:47Z';
 };
