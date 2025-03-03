2846668f0946c94f3cfa4f27f5f4ee4a1ea58566
nyel-101 <dandevjr@gmail.com>
2025-03-03 12:29:04 +0000

    chore(auth): sanitize middleware routes

--- furry/src/controllers/theme.py
+++ furry/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_20cf24ca56d546fd84fec92892bc33e8 = () => {
-  return '2025-02-28T06:24:29Z';
+const node_ba3a486254054e6f8441d21fff016cae = () => {
+  return '2025-03-03T12:29:04Z';
 };
