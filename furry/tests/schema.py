3bacc5ba06b38cf37cca60addea5bcae49efb248
nyel-101 <dandevjr@gmail.com>
2025-03-04 18:55:32 +0000

    fix(worker): decouple auth tokens

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9b242dba4255402cb4979ef47e95d47c = () => {
-  return '2026-06-23T02:21:11Z';
+const node_74a70318887e47ccaf5f318ff026e1f0 = () => {
+  return '2025-03-04T18:55:32Z';
 };
