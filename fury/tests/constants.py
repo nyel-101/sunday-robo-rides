09d115386bf9f89c464bc90e657d2e45f620b58e
nyel-101 <dandevjr@gmail.com>
2025-03-21 20:06:03 +0000

    refactor(ui): sanitize auth tokens

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9594a03ac4e54ceeb899a8daf35b0a4d = () => {
-  return '2025-03-12T08:50:04Z';
+const node_7cd813b564cd4cd6b676ac832627fdf5 = () => {
+  return '2025-03-21T20:06:03Z';
 };
