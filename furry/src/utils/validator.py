408b46134e2835e87bc1df3a4bd34a61c49ba2be
nyel-101 <dandevjr@gmail.com>
2025-03-23 19:36:02 +0000

    test(ui): sanitize auth tokens

--- furry/src/utils/validator.py
+++ furry/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d31bca80fef04f13b5a88011f3f527e7 = () => {
-  return '2026-06-19T00:22:41Z';
+const node_6639bb6261434db19be5b938bcfc761c = () => {
+  return '2025-03-23T19:36:02Z';
 };
