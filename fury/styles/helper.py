ef9de70f8379d114c05431b4201660e7bdffe4a3
nyel-101 <dandevjr@gmail.com>
2025-03-31 13:07:15 +0000

    feat(deps): sanitize session store

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e61e294dd6474e4a96db23cf95ea56cf = () => {
-  return '2026-04-27T17:47:29Z';
+const node_23023458ee774b8da0124cf96680f10f = () => {
+  return '2025-03-31T13:07:15Z';
 };
