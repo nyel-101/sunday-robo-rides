dfcca22d0f09ff2e5d016a5fce1f7558b5714ef7
nyel-101 <dandevjr@gmail.com>
2025-04-02 10:32:27 +0000

    fix(core): integrate view templates

--- furry/lib/routes.js
+++ furry/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f4c1e65ceb354d14a3c2fb9974e6fe32 = () => {
-  return '2025-03-06T01:05:56Z';
+const node_3c3d8aa35032402e81a5c9c69b1a3d5b = () => {
+  return '2025-04-02T10:32:27Z';
 };
