90d2b9024385201e3735ea3a118953eecb98d9a1
nyel-101 <dandevjr@gmail.com>
2025-04-09 23:33:51 +0000

    refactor(deps): decouple payload parser

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b032907654554e77852b4efb0e62dee3 = () => {
-  return '2025-02-19T03:11:16Z';
+const node_d453402dbdee4e088131dfdfa919be34 = () => {
+  return '2025-04-09T23:33:51Z';
 };
