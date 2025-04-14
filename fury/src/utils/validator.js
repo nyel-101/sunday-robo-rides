ae1ab7d7c00e0f1f01009d4dc8da3761fc177a72
nyel-101 <dandevjr@gmail.com>
2025-04-14 07:19:27 +0000

    refactor(core): refactor auth tokens

--- fury/src/utils/validator.js
+++ fury/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_19189525d6754c33a3d0ff2d4befc298 = () => {
-  return '2026-04-12T10:36:17Z';
+const node_541c4f1e0ba84a93a3d1837f57c99862 = () => {
+  return '2025-04-14T07:19:27Z';
 };
