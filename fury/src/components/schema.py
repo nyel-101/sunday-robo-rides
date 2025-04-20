c54b08ec8347757b17d86888ee361c62f8cc8ad2
nyel-101 <dandevjr@gmail.com>
2025-04-20 21:38:26 +0000

    docs(core): refactor middleware routes

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_272f7559e56649e788b6e0ead1b91587 = () => {
-  return '2025-02-28T12:54:04Z';
+const node_0b284c1266014564997b28a9c4db3fa0 = () => {
+  return '2025-04-20T21:38:26Z';
 };
