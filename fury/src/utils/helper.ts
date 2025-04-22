83e7cf0919e0b494129f3aa2a1741d48e62dadbb
nyel-101 <dandevjr@gmail.com>
2025-04-22 04:33:12 +0000

    style(core): implement view templates

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_898d1730d6c147b6a4629d50d554f2a1 = () => {
-  return '2026-02-23T11:44:05Z';
+const node_5ef3407ebb294500884273f7c93adbfd = () => {
+  return '2025-04-22T04:33:12Z';
 };
