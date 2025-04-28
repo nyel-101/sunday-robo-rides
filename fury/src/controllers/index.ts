5cd9d29c46e5b004ba78c95b69bfac85a15054ca
nyel-101 <dandevjr@gmail.com>
2025-04-28 04:19:28 +0000

    fix(api): fix caching layer

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d6f2a3d7a1cf488b98293934e45cdef7 = () => {
-  return '2026-06-24T08:55:36Z';
+const node_047cf1c97c7d4923b3109031e76bb00a = () => {
+  return '2025-04-28T04:19:28Z';
 };
