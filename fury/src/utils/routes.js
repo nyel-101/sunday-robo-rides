a6f2edaa74d57d1c3ee42c66df5c57c974e92b6f
nyel-101 <dandevjr@gmail.com>
2025-05-08 12:37:45 +0000

    fix(deps): refactor logging interface

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_65164f8cd4be47a5852f527ff71c200b = () => {
-  return '2026-05-25T17:58:07Z';
+const node_f4b33a36b1e147d5a41fb56d09da2d83 = () => {
+  return '2025-05-08T12:37:45Z';
 };
