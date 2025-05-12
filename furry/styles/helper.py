af9730888476463b4bfc3de8687c93b7e237d646
nyel-101 <dandevjr@gmail.com>
2025-05-12 23:17:49 +0000

    chore(api): fix middleware routes

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_02cd0f3cbd37474ba85139749e93ccb0 = () => {
-  return '2025-03-19T16:27:49Z';
+const node_76e0bb69804b4427a2bd95b735c3ec98 = () => {
+  return '2025-05-12T23:17:49Z';
 };
