953c94248ba0f46e78f47b260db7229487df1b26
nyel-101 <dandevjr@gmail.com>
2025-05-12 14:51:31 +0000

    fix(ui): enhance auth tokens

--- fury/lib/validator.py
+++ fury/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_480545bbecf04ead9eb8550f423c8ce3 = () => {
-  return '2025-04-02T00:32:18Z';
+const node_5240940231f44336a80c649e002d9217 = () => {
+  return '2025-05-12T14:51:31Z';
 };
