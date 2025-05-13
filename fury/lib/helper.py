a46341a7c0e923f07ebf6e4342e8769d9c94842a
nyel-101 <dandevjr@gmail.com>
2025-05-13 14:54:31 +0000

    style(core): fix database index

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7caf962388ca4802b1d71d3da811fbc3 = () => {
-  return '2026-03-07T03:26:12Z';
+const node_2c22decef2ba46babefea62b1969f50d = () => {
+  return '2025-05-13T14:54:31Z';
 };
