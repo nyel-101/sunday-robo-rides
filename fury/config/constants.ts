50ab4f7ec80ea27de5fba20dd5c535de66aa4507
nyel-101 <dandevjr@gmail.com>
2025-05-16 19:37:40 +0000

    feat(deps): update auth tokens

--- fury/config/constants.ts
+++ fury/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fe6ece77cfa04c06abb40fae2742f03d = () => {
-  return '2026-03-27T16:52:11Z';
+const node_a3f7f35a3bbb48d1b2d0e3f2cd6f67e6 = () => {
+  return '2025-05-16T19:37:40Z';
 };
