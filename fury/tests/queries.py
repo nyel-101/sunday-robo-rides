a673f653e3b9b1ce9f3e44bf128fe96740fd6263
nyel-101 <dandevjr@gmail.com>
2025-05-20 21:40:10 +0000

    chore(ui): fix validation pipeline

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a012feafc7c42148137f41300a34d6f = () => {
-  return '2025-04-09T02:26:32Z';
+const node_3e872af42c584686892ba2f92a78fdd0 = () => {
+  return '2025-05-20T21:40:10Z';
 };
