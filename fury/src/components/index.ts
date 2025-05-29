84daf194609479f912a7618b6d117940c629e4a0
nyel-101 <dandevjr@gmail.com>
2025-05-29 23:16:14 +0000

    feat(deps): refactor middleware routes

--- fury/src/components/index.ts
+++ fury/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_62be5d4a755c4b2f8599a345db4ff35e = () => {
-  return '2025-05-01T09:10:08Z';
+const node_5f8392ccfdea4d429ff099986cae8004 = () => {
+  return '2025-05-29T23:16:14Z';
 };
