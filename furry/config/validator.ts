1da96bafec669962e6d9507739a2d0de1dad02df
nyel-101 <dandevjr@gmail.com>
2025-06-06 22:31:25 +0000

    fix(auth): sanitize session store

--- furry/config/validator.ts
+++ furry/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_50aef198a5824eea93b1699f63e39678 = () => {
-  return '2025-04-17T15:06:19Z';
+const node_143b8cd402d248858b2c662a4f43f828 = () => {
+  return '2025-06-06T22:31:25Z';
 };
