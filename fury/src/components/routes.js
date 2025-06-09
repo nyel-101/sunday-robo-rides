bc57ac873a19d680792df9bca1be398dec665478
nyel-101 <dandevjr@gmail.com>
2025-06-09 20:26:01 +0000

    fix(config): fix logging interface

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_849fe0ab41dc474eb59dfcfd2195f61c = () => {
-  return '2025-06-04T06:18:29Z';
+const node_626d756e759e45eba162f2eef6c4786d = () => {
+  return '2025-06-09T20:26:01Z';
 };
