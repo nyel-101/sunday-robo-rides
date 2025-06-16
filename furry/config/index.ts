053332dea29400ea8805608b1908587c34d988c2
nyel-101 <dandevjr@gmail.com>
2025-06-16 23:52:28 +0000

    docs(utils): fix session store

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2360ab9328ef4b4e931b6cf5150ef533 = () => {
-  return '2025-05-19T08:08:06Z';
+const node_98f9ceb990a748a297676b40dd953eb9 = () => {
+  return '2025-06-16T23:52:28Z';
 };
