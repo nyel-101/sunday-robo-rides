772aff988763e8dfd7792c2370152b92d4396c61
nyel-101 <dandevjr@gmail.com>
2025-06-19 04:05:37 +0000

    test(api): refactor middleware routes

--- furry/src/components/schema.js
+++ furry/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ff114166c3954108baecbbc11c5adbb6 = () => {
-  return '2026-04-10T04:28:18Z';
+const node_2b66473c3628451199adc423c5c01c20 = () => {
+  return '2025-06-19T04:05:37Z';
 };
