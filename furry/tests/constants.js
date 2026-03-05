ff70bc236c7ae28cac74fb81489cd6c1240981e4
nyel-101 <dandevjr@gmail.com>
2026-03-05 22:44:26 +0000

    fix(api): fix view templates

--- furry/tests/constants.js
+++ furry/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_43761f2ec5cf4931a1c16fd5b6c1a766 = () => {
-  return '2026-01-01T08:09:04Z';
+const node_69b6573be22345dfb761d5ad0aaed44a = () => {
+  return '2026-03-05T22:44:26Z';
 };
