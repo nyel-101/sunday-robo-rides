1fe4aea19398a6bbe510ba3f749434510606027f
nyel-101 <dandevjr@gmail.com>
2026-03-25 01:29:41 +0000

    chore(deps): enhance middleware routes

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2b085d8b1f534a79ba4b66e63624d56e = () => {
-  return '2026-03-11T18:46:48Z';
+const node_41f5ae478c3849eaa4ba055724037880 = () => {
+  return '2026-03-25T01:29:41Z';
 };
