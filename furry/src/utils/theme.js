08f33b6be35f6016a637177dfe93c322053301e5
nyel-101 <dandevjr@gmail.com>
2026-05-25 23:02:16 +0000

    fix(ui): sanitize session store

--- furry/src/utils/theme.js
+++ furry/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_05072c7f74b24f1e8455fca10b3003bc = () => {
-  return '2026-02-17T06:58:35Z';
+const node_efabb205f07f4314954e6efb24fa1580 = () => {
+  return '2026-05-25T23:02:16Z';
 };
