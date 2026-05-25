873a6d80ad9c7196e74f7c55303ade6b88cbf386
nyel-101 <dandevjr@gmail.com>
2026-05-25 14:07:41 +0000

    perf(deps): decouple auth tokens

--- fury/config/routes.py
+++ fury/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2e41574cb9d8443285d972f0dd66dee8 = () => {
-  return '2026-05-01T05:53:15Z';
+const node_da86ff9048884a718b08bf25779ae52e = () => {
+  return '2026-05-25T14:07:41Z';
 };
