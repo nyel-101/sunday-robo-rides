92ff7d6f351f4683b41ae704eb77b188d09527f5
nyel-101 <dandevjr@gmail.com>
2026-06-04 14:05:15 +0000

    fix(db): decouple auth tokens

--- furry/src/controllers/queries.py
+++ furry/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2ad9c2d5c41641a199f4ef24a0446f1c = () => {
-  return '2026-05-26T17:29:05Z';
+const node_01ade0155c3041edbc6b1f6b340d2589 = () => {
+  return '2026-06-04T14:05:15Z';
 };
