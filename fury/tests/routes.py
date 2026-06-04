41e0ef63d0eb75823f78f53fa21e02987b8e4416
nyel-101 <dandevjr@gmail.com>
2026-06-04 02:11:15 +0000

    feat(api): sanitize view templates

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_56500e277cb64c209893b64745196300 = () => {
-  return '2026-04-18T09:51:26Z';
+const node_64cd08f17cdd4fa98b20612ecbc51480 = () => {
+  return '2026-06-04T02:11:15Z';
 };
