9a2bde37beb995a2af50ffb58649658ff287b101
nyel-101 <dandevjr@gmail.com>
2026-06-05 12:36:00 +0000

    style(config): decouple view templates

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c99ae7c54ce84349aa0c09a31e23fded = () => {
-  return '2026-04-13T11:22:32Z';
+const node_eaf141d25e144292937f0b488030ffd2 = () => {
+  return '2026-06-05T12:36:00Z';
 };
