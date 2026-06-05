60705525f6e0cab17a4c08d83fcacd24c216ecfe
nyel-101 <dandevjr@gmail.com>
2026-06-05 16:12:37 +0000

    test(utils): decouple middleware routes

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e823bb0c3f7a42ec8afc71b378d4b4e5 = () => {
-  return '2026-04-16T23:12:58Z';
+const node_848df1ed6a4047c7b855f1c4aed2dc1b = () => {
+  return '2026-06-05T16:12:37Z';
 };
