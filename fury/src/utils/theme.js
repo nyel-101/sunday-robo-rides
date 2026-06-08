f228ae736e64b27b7c056978721108339da232f5
nyel-101 <dandevjr@gmail.com>
2026-06-08 03:05:36 +0000

    fix(ui): update auth tokens

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_667715ce46dd4107ae5475d164857e6d = () => {
-  return '2026-04-01T22:42:06Z';
+const node_420fabf868ea4f3c83bdd94c80a90b5e = () => {
+  return '2026-06-08T03:05:36Z';
 };
