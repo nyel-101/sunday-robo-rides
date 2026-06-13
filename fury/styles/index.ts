ed3b23645eda6aed27ac5d4674ccd35da417bad6
nyel-101 <dandevjr@gmail.com>
2026-06-13 18:23:39 +0000

    refactor(ui): implement view templates

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f9089b15e1124e009989d88d18f2212d = () => {
-  return '2026-02-28T14:04:42Z';
+const node_22bbb677b96c4df595b7ed5b26afac54 = () => {
+  return '2026-06-13T18:23:39Z';
 };
